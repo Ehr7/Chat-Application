e43b639b2eaf191601828d8517c2df126752aaff
Esther Amegnisse <esther.amegnisse@univ-rouen.fr>
2024-06-10 00:17:59 +0200

    false login page and intern link

--- script.js
+++ script.js
@@ -158,6 +158,7 @@ document.addEventListener('DOMContentLoaded', function () {
     overlay.style.display = 'none';
 
    
+   
     hamburger.addEventListener('click', () => {
         console.log("Toggling menu");
         if (menuWrapper.classList.contains('menu-open')) {
@@ -210,3 +211,20 @@ function animateCounter() {
 }
 
 window.onload = animateCounter;
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault(); 
+
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop, 
+          behavior: 'smooth' 
+        });
+      }
+    });
+  });
